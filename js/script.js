d3aa36d6b3342e35afc85ab1729a2a0b98ef35de
Verta-master <vertyagina@gmail.com>
2018-06-26 15:56:23 +0600

    Update 2018-06-26T09:56:19.079Z

--- js/script.js
+++ js/script.js
@@ -2,17 +2,10 @@ jQuery(function($) {
 	"use strict";
 
 //Mobile menu
-$('.menu__btn').click(function() {
-  $('.menu').toggleClass('menu--opened');
-  $('.menu__list').slideToggle();
+$('.header__btn').click(function() {
+  $('.header__nav').slideToggle();
+  $(this).toggleClass('header__btn--opened');
 })
-
-if (window.innerWidth < 766) {
-  $('.menu__link').click(function() {
-    $('.menu__list').slideToggle();
-    $('.menu').removeClass('menu--opened');
-  })
-}
 
 //Scroll to top
 $(window).scroll(function(){
